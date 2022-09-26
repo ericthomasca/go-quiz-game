70af5f4fb77a8403aa0c537e63e52acff4d892be
Eric Thomas <eric@ericthomas.ca>
2022-09-26 19:42:45 -0230

    read csv file and ignore executable

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	csvFileName := flag.String("csv", "problems.csv", "A string with the filename of the CSV including extension")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+
+	if err != nil {
+		fmt.Printf("Could not find file named %s\n", *csvFileName)
+		os.Exit(1)
+	}
+
+	csv_data := csv.NewReader(file)
+
+	problems, err := csv_data.ReadAll()
+
+	if err != nil {
+		fmt.Println("Problem with CSV data")
+		os.Exit(1)
+	}
+
+	for _, problem := range problems {
+		fmt.Println(problem)
+	}
+
 }
