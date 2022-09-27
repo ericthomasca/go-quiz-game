d526873795592860b6c1289d3f8f50297d69812b
Eric Thomas <eric@ericthomas.ca>
2022-09-26 22:46:54 -0230

    print question data

--- main.go
+++ main.go
@@ -7,12 +7,27 @@ import (
 	"os"
 )
 
+type problem struct {
+	question string
+	answer string
+}
+
+func parseLines(lines [][]string) []problem {
+	problemChan := make([]problem, len(lines))
+	for i, line := range lines {
+		problemChan[i] = problem{
+			question: line[0],
+			answer: line[1],
+		}
+	}
+	return problemChan
+}
+
 func main() {
 	csvFileName := flag.String("csv", "problems.csv", "A string with the filename of the CSV including extension")
 	flag.Parse()
 
 	file, err := os.Open(*csvFileName)
-
 	if err != nil {
 		fmt.Printf("Could not find file named %s\n", *csvFileName)
 		os.Exit(1)
@@ -20,15 +35,16 @@ func main() {
 
 	csv_data := csv.NewReader(file)
 
-	problems, err := csv_data.ReadAll()
-
+	lines, err := csv_data.ReadAll()
 	if err != nil {
 		fmt.Println("Problem with CSV data")
 		os.Exit(1)
 	}
 
-	for _, problem := range problems {
-		fmt.Println(problem)
+	problems := parseLines(lines)
+
+	for i, problem := range problems {
+		fmt.Printf("Question %d: %s = %s\n", i+1, problem.question, problem.answer)
 	}
 
 }
